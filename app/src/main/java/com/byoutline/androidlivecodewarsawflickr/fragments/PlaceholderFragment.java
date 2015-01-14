a027c675552e70690f04830a7e336d678c2e6849
Sebastian Kacprzak <nait@naitbit.com>
2015-01-14 16:59:48 +0100

    Added: empty RecyclerView.

--- app/src/main/java/com/byoutline/androidlivecodewarsawflickr/fragments/PlaceholderFragment.java
+++ app/src/main/java/com/byoutline/androidlivecodewarsawflickr/fragments/PlaceholderFragment.java
@@ -1,8 +1,11 @@
 package com.byoutline.androidlivecodewarsawflickr.fragments;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +13,17 @@ import android.view.ViewGroup;
 import com.byoutline.androidlivecodewarsawflickr.R;
 import com.byoutline.androidlivecodewarsawflickr.activities.MainActivity;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
+
+    @InjectView(R.id.photos_list_view)
+    RecyclerView photoRV;
+
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -39,6 +49,8 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.inject(this, rootView);
+        setUpAdapters(container.getContext());
         return rootView;
     }
 
@@ -47,5 +59,11 @@ public class PlaceholderFragment extends Fragment {
         super.onAttach(activity);
         ((MainActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    private void setUpAdapters(Context context) {
+        photoRV.setLayoutManager(new LinearLayoutManager(context));
+        photoRV.setHasFixedSize(true);
+//        photoRV.setAdapter(photosAdapter);
     }
 }
