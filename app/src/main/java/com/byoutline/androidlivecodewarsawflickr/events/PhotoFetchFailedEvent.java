b909e5180d328fd3e1783abb239230158ef1a59f
Sebastian Kacprzak <nait@naitbit.com>
2015-01-15 13:39:33 +0100

    Added: forgotten event files.

---
+++ app/src/main/java/com/byoutline/androidlivecodewarsawflickr/events/PhotoFetchFailedEvent.java
@@ -0,0 +1,7 @@
+package com.byoutline.androidlivecodewarsawflickr.events;
+
+/**
+ * @author Sebastian Kacprzak <nait at naitbit.com>
+ */
+public class PhotoFetchFailedEvent {
+}
---
+++ app/src/main/java/com/byoutline/androidlivecodewarsawflickr/events/RecentPhotosFetchedEvent.java
@@ -0,0 +1,10 @@
+package com.byoutline.androidlivecodewarsawflickr.events;
+
+import com.byoutline.androidlivecodewarsawflickr.model.FlickrResponse;
+import com.byoutline.eventcallback.ResponseEventImpl;
+
+/**
+ * Created by Sebastian Kacprzak on 10.01.15.
+ */
+public class RecentPhotosFetchedEvent extends ResponseEventImpl<FlickrResponse> {
+}
